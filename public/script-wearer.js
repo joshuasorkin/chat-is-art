ac30e0ad5506681a5d3c15e3e4d853a5a1ca1355
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-06-01 22:39:56 -0700

    changes to CSS for resizing text

--- public/script-wearer.js
+++ public/script-wearer.js
@@ -4,39 +4,11 @@ const socket = io();
 //from the DOM
 //const chatWindow = document.querySelector('.chat-text');
 const chatText = document.getElementById('chat-text');
-const chatSvg = document.getElementById('chat-svg');
 
-function setSvg(){
-    console.log("setting svg...");
-    console.log(window.innerWidth);
-    console.log(window.innerHeight);
-    chatSvg.setAttribute("width",window.innerWidth);
-    chatSvg.setAttribute("height",window.innerHeight);
-    //chatText.setAttribute("textLength",window.innerWidth);
-    resizeText();
-}
-
-function resizeText(){
-    var bbox = chatText.getBBox();
-    //chatText.setAttribute("textLength",window.innerWidth/10);
-    chatSvg.get(0).setAttribute('viewBox', 
-    [bbox.x,
-     bbox.y,
-     bbox.width,
-     bbox.height].join(' '));
-    /*
-    var widthTransform = window.innerWidth / bb.width;
-    var heightTransform = window.innerHeight / bb.height;
-    var value = widthTransform < heightTransform ? widthTransform : heightTransform;
-    chatText.setAttribute("transform", "matrix("+value+", 0, 0, "+value+", 0,0)");
-    */
-    //chatText.setAttribute("textLength",window.innerWidth);
-}
 
 
 const renderMessage = message => {
     chatText.innerHTML = message;
-    resizeText();
 }
 
 //add event listener for receiving 'chat' message from server
