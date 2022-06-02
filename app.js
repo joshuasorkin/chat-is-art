09c22d1f092ca35e00e100a64333c6f7d1ba2167
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-06-02 15:54:37 -0700

    add QR code to chat page

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ io.on('connection',socket=>{
     //get 'chat' event from client and broadcast the message
     socket.on('chat',message =>{
 
-        message=mentionChecker.formatMentions(message);
+        //message=mentionChecker.formatMentions(message);
         var broadcastMessage;
         broadcastMessage=`${socket.username}: ${message}`;
         io.emit('chat',broadcastMessage);
