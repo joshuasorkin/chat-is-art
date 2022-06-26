0465933e1e7cab1348d005053acbb12ab08fdf38
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-06-25 21:11:59 -0700

    Revert "remove second fs declare"
    This reverts commit 5192b0d66b5f2980c8d90dce6869e482ddb6d00d.

--- routes/emergency.js
+++ routes/emergency.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
