b1e9261235d66b39bf0a9194d565ef60f5fb6d64
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-06-25 21:15:51 -0700

    broken emergency

--- routes/emergency.js
+++ routes/emergency.js
@@ -2,17 +2,10 @@ const fs = require('fs')
 
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-        fs.writeFile('../log.txt', '')
-        .then(result=>{
-            res.send(`log file cleared.  ${result}`);
-        })
-        .catch(err=>{
-            res.send(`failed to clear log; ${err}`);
-        });
+    fs.writeFile('../log.txt', '', function(){console.log('done')})
 });
 
 module.exports = router;
