5192b0d66b5f2980c8d90dce6869e482ddb6d00d
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-06-25 21:08:53 -0700

    remove second fs declare

--- routes/emergency.js
+++ routes/emergency.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
